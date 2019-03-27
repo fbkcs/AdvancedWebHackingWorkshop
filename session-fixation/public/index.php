7af7b98463206dc7d7cf5f5d353ca52d29f5ecfa
Mikhail Firstov <mfirstov@fbkcs.ru>
2019-03-27 17:52:10 +0300

    Session Fixation added
    Added example for Session Fixation

---
+++ session-fixation/public/index.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+$userinfo = array(
+                'admin'=>'123123',
+                'root'=>'qweqwe'
+                );
+
+if(isset($_GET['logout'])) {
+    $_SESSION['username'] = '';
+    header('Location:  ' . $_SERVER['PHP_SELF']);
+}
+
+if(isset($_POST['username'])) {
+    if($userinfo[$_POST['username']] == $_POST['password']) {
+        $_SESSION['username'] = $_POST['username'];
+    }else {
+        echo "Bad login!";
+    }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <?php if($_SESSION['username']): ?>
+            <p>You are logged in as <?=$_SESSION['username']?></p>
+            <p><a href="?logout=1">Logout</a></p>
+        <?php endif; ?>
+        <form name="login" action="" method="post">
+            Username:  <input type="text" name="username" value="" /><br />
+            Password:  <input type="password" name="password" value="" /><br />
+            <input type="submit" name="submit" value="Submit" />
+        </form>
+    </body>
+</html>
---
+++ session-fixation/public/setcookie.php
@@ -0,0 +1,15 @@
+Set cookies: 
+
+<form action="" method="GET">
+key:  <input type="text" name="key" value="" /><br />
+value:  <input type="text" name="value" value="" /><br />
+<input type="submit" name="submit" value="Submit" />
+</form>
+
+<?php
+
+if (isset($_GET['key']) && isset($_GET['value'])) {
+	setcookie($_GET['key'], $_GET['value']);
+}
+
+?>
