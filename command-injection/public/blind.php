e46ee2bd9c57596871476c45f3d2ab3400accf1f
Andrey <askuratov@fbkcs.ru>
2019-03-27 16:55:25 +0300

    Command injection examples

---
+++ command-injection/public/blind.php
@@ -0,0 +1,24 @@
+<?php
+
+function curl($target){
+	shell_exec('curl -vvv -L -sD - -o /dev/null '.$target );
+	echo 'We submitted your curl for future!';
+}
+
+if (isset($_GET['curl']) && !empty($_GET['curl'])){
+	curl($_GET['curl']);
+}
+else{
+	echo '
+<!DOCTYPE html>
+<html>
+<body>
+<form>
+  Curl it:<br>
+  <input type="text" name="curl"><br>
+  <input type="submit" value="GO!">
+</form> 
+</body>
+</html>';
+
+}
